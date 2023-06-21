77448d70a986d7a6870cffc3907f8d71c946e72a
JoaoBernardoVeiga <83598646+JoaoBernardoVeiga@users.noreply.github.com>
2023-06-21 18:17:42 +0100

    Add createArea

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-import Footer from "./Footer";
-import Header from "./Header";
-import Note from "./Note";
-import notes from "../notes";
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map((note) => (
-        <Note key={note.key} title={note.title} content={note.content} />
-      ))}
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
