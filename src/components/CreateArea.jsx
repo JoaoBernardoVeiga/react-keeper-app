c109295cbaf3ad1814c5d2e6b07795c168aea943
JoaoBernardoVeiga <83598646+JoaoBernardoVeiga@users.noreply.github.com>
2023-06-22 13:06:56 +0100

    Custom Notes: Add Notes, Delete Notes Functionalities

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [noteState, SetNoteState] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+    SetNoteState((prevNoteState) => {
+      return {...prevNoteState,
+        [name]: value
+      }
+    });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          value={noteState.title}
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+        />
+        <textarea
+          value={noteState.content}
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            props.AddNoteFunction(noteState);
+            SetNoteState({ title: "", content: "" });
+          }}
+        >
+          Add
+        </button>
       </form>
     </div>
   );
